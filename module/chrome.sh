b5001e1e61f0fa916bfd74ff252ef6bb9e417ecc
yuu <yuu@localhost.localdomain>
2024-02-11 18:59:51 +0900

    update module/chrome.sh

--- module/chrome.sh
+++ module/chrome.sh
@@ -1,4 +1,12 @@
 #!/bin/sh
 #chrome
-echo "installing Google chrome!"
-
+sudo bash -c 'cat <<end > /etc/yum.repos.d/google-chrome.repo
+[google-chrome]
+name=google-chrome
+baseurl=http://dl.google.com/linux/chrome/rpm/stable/\$basearch
+enable=1
+gpgcheck=1
+gpgkey=https://dl-ssl.google.com/linux/linux_signing_key.pub
+end'
+sudo yum -y update
+sudo yum install -y google-chrome-stable
