a1806c4208f9cc7d9d17f8d5cdcad3f48f933acc
yuu <yuu@localhost.localdomain>
2024-02-11 13:04:10 +0900

    initial_setup.sh update

--- initial_setup.sh
+++ initial_setup.sh
@@ -37,6 +37,12 @@ gsettings set org.gnome.desktop.wm.preferences button-layout 'appmenu:minimize,m
 #nodejs
 sudo yum install -y nodejs
 
+#VScodeインストール
+sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
+sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
+sudo dnf check-update
+sudo dnf install -y code
+
 #capslockをctrlとして使う
 sudo tee /etc/X11/xorg.conf.d/00-keyboard.conf <<EOF
 Section "InputClass"
