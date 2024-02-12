3091e377530870c5a388b2fc86b9d2a8c3af66fd
yuu <ydiangang82@gmail.com>
2024-02-12 14:00:39 +0900

    update ========================== ./module/git.sh ==========================

--- module/git.sh
+++ module/git.sh
@@ -6,8 +6,3 @@ read -p "input your git username : " gitusername
 read -p "input your git email : " gitemail
 git config --global user.name "$gitusername"
 git config --global user.email "$gitemail"
-
-mkdir ~/.ssh
-chmod 700 ~/.ssh
-#確認
-ssh -T git@github.com
